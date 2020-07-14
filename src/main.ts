dd0aab8c1a07a3d87d9695298c67cbe7391f1fa0
rinsuki <428rinsuki+git@gmail.com>
2020-07-14 16:39:57 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { processConnection } from "./process-connection.ts";
+import { config } from "./config.ts";
 
-const listener = Deno.listen({ port: 443 });
+const listener = Deno.listen({ port: config.port });
 for await (const conn of listener) {
   processConnection(conn).catch((e) => {
     if (e instanceof Error && e.name === "ConnectionReset") return;
